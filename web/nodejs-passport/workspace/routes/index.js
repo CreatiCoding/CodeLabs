d6b354a2ebe39b24352b95ad212acf0329ae39e6
creaticoding <creaticoding@gmail.com>
2018-11-29 18:54:03 +0900

    feat(route): add login get and post function

--- web/nodejs-passport/workspace/routes/index.js
+++ web/nodejs-passport/workspace/routes/index.js
@@ -1,6 +1,22 @@
 var express = require("express");
 var router = express.Router();
 
+// TODO: post login
+router.post(
+  "/login",
+  self.passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/login"
+  })
+);
+
+/* GET login page. */
+router.get("/login", function(req, res, next) {
+  res.render("login", {
+    title: "Login to Tutorial Nodejs Passport"
+  });
+});
+
 /* GET home page. */
 router.get("/home", function(req, res, next) {
   // temporary code
@@ -20,13 +36,6 @@ router.get("/home", function(req, res, next) {
 /* GET root page. */
 router.get("/", function(req, res, next) {
   res.redirect("/");
-});
-
-/* GET login page. */
-router.get("/login", function(req, res, next) {
-  res.render("login", {
-    title: "Login to Tutorial Nodejs Passport"
-  });
 });
 
 module.exports = router;
